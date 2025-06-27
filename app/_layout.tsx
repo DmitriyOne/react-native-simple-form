722abcc446ddeb374f4b3c835611aec2256665d3
DmitriyOne <senkoner4@gmail.com>
2025-06-27 15:28:17 +0100

    Animate splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,15 @@ import { FONT_FAMILY } from "@/src/06_shared/config/fonts"
 import { useFonts } from "expo-font"
 import { Stack } from "expo-router"
 import * as SplashScreen from "expo-splash-screen"
-import { useEffect } from "react"
-import { StyleSheet } from "react-native"
+import { useEffect, useRef } from "react"
+import { Animated, StyleSheet } from "react-native"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const fadeAnim = useRef(new Animated.Value(0)).current
+
   const [loaded, error] = useFonts({
     [FONT_FAMILY.montserrat_regular]: require("../assets/fonts/Montserrat-Regular.ttf"),
     [FONT_FAMILY.montserrat_bold]: require("../assets/fonts/Montserrat-Bold.ttf"),
@@ -17,7 +19,13 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (loaded || error) {
-      SplashScreen.hideAsync()
+      SplashScreen.hideAsync().then(() => {
+        Animated.timing(fadeAnim, {
+          toValue: 0,
+          duration: 500,
+          useNativeDriver: true,
+        }).start()
+      })
     }
   }, [loaded, error])
 
@@ -27,23 +35,28 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.saveArea}>
-        <Stack>
-          <Stack.Screen
-            name='(tabs)'
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name='form'
-            options={{ headerShown: false }}
-          />
-        </Stack>
-      </SafeAreaView>
+      <Animated.View style={styles.animatedContainer}>
+        <SafeAreaView style={styles.saveArea}>
+          <Stack>
+            <Stack.Screen
+              name='(tabs)'
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name='form'
+              options={{ headerShown: false }}
+            />
+          </Stack>
+        </SafeAreaView>
+      </Animated.View>
     </SafeAreaProvider>
   )
 }
 
 const styles = StyleSheet.create({
+  animatedContainer: {
+    flex: 1,
+  },
   saveArea: {
     flex: 1,
     backgroundColor: COLORS.white,
