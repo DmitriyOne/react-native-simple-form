df148eba00cd79217b309b9912b2a2a60471e90b
DmitriyOne <senkoner4@gmail.com>
2025-06-28 19:17:49 +0100

    Delete unwanted entities

--- src/05_entities/user/lib/validators.ts
+++
@@ -1,7 +0,0 @@
-export const validateInn = (inn: string) => {
-  return /^\d{12}$/.test(inn)
-}
-
-export const validateResidency = (residency: string | null) => {
-  return residency === 'yes' || residency === 'no'
-}
