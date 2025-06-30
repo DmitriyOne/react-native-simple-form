445eaa5f48394942a4bc09319d1593d666e337b7
DmitriyOne <senkoner4@gmail.com>
2025-06-30 20:28:12 +0100

    Create page with_react_hook_form

--- app/(tabs)/soon-form.tsx
+++ app/(tabs)/soon-form.tsx
@@ -1,39 +1,5 @@
-import { COLORS, TEXT_SIZE, TITLE_SIZE } from "@/src/06_shared/config/constants"
-import { FONT_FAMILY } from "@/src/06_shared/config/fonts"
-import { paths } from "@/src/06_shared/config/routing"
-import { Link } from "@/src/06_shared/ui/link"
-import { StyleSheet, Text, View } from "react-native"
+import { SoonFormPage } from "@/src/02_pages/soon-form"
 
 export default function SoonCreateWallet() {
-  return (
-    <View style={styles.component}>
-      <Text style={styles.title}>Your form soon!</Text>
-      <Link
-        href={paths.create_wallet}
-        text='Click me and create your wallet!'
-        styles={{
-          text: styles.text,
-        }}
-      />
-    </View>
-  )
+  return <SoonFormPage />
 }
-
-const styles = StyleSheet.create({
-  component: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: COLORS.white,
-  },
-  title: {
-    fontFamily: FONT_FAMILY.montserrat_bold,
-    fontSize: TITLE_SIZE.medium,
-    paddingBottom: 4,
-  },
-  text: {
-    fontFamily: FONT_FAMILY.montserrat_regular,
-    fontSize: TEXT_SIZE.small,
-    textDecorationLine: "underline",
-  },
-})
---
+++ app/create-wallet-with-react-hook-form.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function CreateWalletWithReactHookForm() {
+  return <Text>Create wallet with react hook form</Text>
+}
