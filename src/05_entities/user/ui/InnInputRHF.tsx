4f1fb0a5d45977ae14e89ee6c1b57d0d7ccbe979
DmitriyOne <senkoner4@gmail.com>
2025-07-01 17:47:15 +0100

    Fix rhf input

--- src/05_entities/user/ui/InnInputRHF.tsx
+++ src/05_entities/user/ui/InnInputRHF.tsx
@@ -1,8 +1,19 @@
 import { InputRHF, TInputRHFProps } from "@/src/06_shared/ui/input-rhf"
 import { FieldValues } from "react-hook-form"
+import { Keyboard } from "react-native"
 
-export const InnInputRHF = <T extends FieldValues>({
-  ...props
-}: TInputRHFProps<T>) => {
-  return <InputRHF<T> {...props} />
+type TProps<T extends FieldValues> = Omit<
+  TInputRHFProps<T>,
+  "onSubmitEditing" | "returnKeyType" | "keyboardType"
+> & {}
+
+export const InnInputRHF = <T extends FieldValues>({ ...props }: TProps<T>) => {
+  return (
+    <InputRHF<T>
+      onSubmitEditing={() => Keyboard.dismiss()}
+      returnKeyType='done'
+      keyboardType='numeric'
+      {...props}
+    />
+  )
 }
